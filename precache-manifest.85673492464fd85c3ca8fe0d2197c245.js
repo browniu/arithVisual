cb1dd6f1177afcadd6804cc914abb3bd11d49c08
browniu <niuniusoga@qq.com>
2019-10-20 18:28:37 +0800

    Updates

---
+++ precache-manifest.85673492464fd85c3ca8fe0d2197c245.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8694ddbaa6cd4f9e903c5f8724c27afe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac5b7f3f5c60fb94702d",
+    "url": "/static/css/main.913abbc4.chunk.css"
+  },
+  {
+    "revision": "6c26ea2e2519a76c80a4",
+    "url": "/static/js/2.1d0729d5.chunk.js"
+  },
+  {
+    "revision": "ac5b7f3f5c60fb94702d",
+    "url": "/static/js/main.00db5e2a.chunk.js"
+  },
+  {
+    "revision": "297faa65a6607a58c29e",
+    "url": "/static/js/runtime-main.c8e8bfa0.js"
+  }
+]);
